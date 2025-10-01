6835e58b665667bd591e96ae65d60c360f75eb36
ismael-valentao <chizianeismaelvalentao@gmail.com>
2025-10-01 11:19:33 +0200

    atualizacao da taxa de cotacao

--- src/controllers/quotationController.js
+++ src/controllers/quotationController.js
@@ -1,9 +1,13 @@
 const Quotation = require('../models/quotation');
 const { generateQuotationPDF } = require('../utils/pdfGenerator');
-const { amounts } = require('../utils/amountCalculator');
+const { amounts, normalizeItems } = require('../utils/amountCalculator');
 
 exports.createQuotation = async (req, res) => {
-    const { subTotal, tax, totalAmount } = amounts(req.body.items, req.body.iva*1*0.1);
+    const { subTotal, tax, totalAmount } = amounts(req.body.items, req.body.iva * 1 * 0.01);
+    console.log(req.body.iva*1*0.1)
+    const cleanedItems = normalizeItems(req.body.items);
+    console.log(subTotal, tax, totalAmount)
+    console.log(req.body.items)
     const userId = req.user._id;
     const companyId = req.user.company._id;
     const quotation = new Quotation({
@@ -12,8 +16,8 @@ exports.createQuotation = async (req, res) => {
         clientNUIT: req.body.clientNUIT,
         quotationNumber: req.body.quotationNumber,
         date: req.body.date,
-        items: req.body.items,
-        appliedTax:req.body.iva*1*0.01,
+        items: cleanedItems,
+        appliedTax: req.body.iva * 1 * 0.01,
         subTotal,
         tax,
         totalAmount,
@@ -23,7 +27,7 @@ exports.createQuotation = async (req, res) => {
     });
 
     await quotation.save();
-    res.status(201).json({ status:"success" , Quotation });
+    res.status(201).json({ status: "success", Quotation });
 };
 
 exports.getQuotations = async (req, res) => {
@@ -47,7 +51,7 @@ exports.getQuotationById = async (req, res) => {
 exports.getLastQuotation = async (req, res) => {
     const companyId = req.user.company._id;
     const lastQuotation = await Quotation.findOne({ companyId }).sort({ createdAt: -1 }).limit(1);
-    
+
     return res.status(200).json({ success: true, lastQuotation: !lastQuotation ? '000' : lastQuotation.quotationNumber });
 }
 
